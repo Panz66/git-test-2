fd0bf61c110dc65e089189740414bede37e3405f
Ipan Wardana <ipanwardana123@gmail.com>
2023-11-26 21:09:36 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quam explicabo modi delectus, nostrum beatae esse aliquid nisi error sapiente quis inventore incidunt, sit numquam. Incidunt qui et eligendi ipsam!</p>
 </body>
 </html>
